0d1c17e5355577b7786b3d63e840f53035091666
superhsu <31141265+superhsu@users.noreply.github.com>
2018-03-15 17:10:10 -0700

    10m added to mongo

--- database/models/fake/reviews-generator.js
+++ database/models/fake/reviews-generator.js
@@ -1,10 +1,12 @@
 const faker = require('faker');
 const fs = require('fs');
+const db = require('../index.js');
+const MongoClient = require('mongodb').MongoClient;
 
 const generateReviews = () => {
   // load 200, create 
   const guests = [];
-  for (let i = 1; i < 201; i++) {
+  for (let i = 1; i < 5; i++) {
     let User = {
       id: i,
       reviews: {
@@ -62,7 +64,7 @@ const generateMessages = () => {
 
 const createAndWriteToFile = () => {
   let data = generateReviews();
-  let filename = 'mocked_data.js';
+  let filename = 'mocked_data3.js';
 
   fs.writeFile(filename, JSON.stringify(data), (err) => {
     if (err) {
@@ -73,4 +75,37 @@ const createAndWriteToFile = () => {
   });
 };
 
-createAndWriteToFile()
+// createAndWriteToFile()
+
+  async function milReviews() {
+    const clientConnect = await MongoClient.connect('mongodb://localhost/27017');
+    const mdb = clientConnect.db('reviews');
+    const collection = mdb.collection('guests');
+    var guests = [];
+    for (var i = 0; i <= 10000000; i++) {
+      if (i !== 0 && i % 100000 === 0) {
+        await collection.insertMany(guests);
+        console.log(`100000! count ${i/100000}`); 
+        guests = [];     
+      }
+      let user = {
+        _id: i,
+        reviews: {
+          guest_name: faker.name.findName(),
+          communication: faker.random.number({ min: 0, max: 5 }),
+          cleaniness: faker.random.number({ min: 0, max: 5 }),
+          location: faker.random.number({ min: 0, max: 5 }),
+          checkin: faker.random.number({ min: 0, max: 5 }),
+          accuracy: faker.random.number({ min: 0, max: 5 }),
+          message: faker.hacker.phrase(),
+          date: faker.date.month(),
+          image: faker.image.avatar(),
+          value: faker.random.number({ min: 0, max: 5 })
+        }
+      }
+      guests.push(user);
+    }
+    clientConnect.close();
+  }
+
+  milReviews();
